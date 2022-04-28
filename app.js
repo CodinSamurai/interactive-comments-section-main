81ec5457171790e0a3c8de779f76df19c9199705
TonyBod <bodudeanthony@gmail.com>
2022-04-28 14:25:06 -0700

    update

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var increase = document.querySelectorAll('.increase');
 var decrease = document.querySelectorAll('.decrease');
-var inner = document.querySelectorAll('.result');
+var result = document.querySelectorAll('.result');
 
 increase.forEach(function(hello){
-    hello.addEventListener('click', function(){
+    hello.addEventListener('click', function () {
         hello.style.backgroundColor = 'green';
         console.log(hello);
         // res1++;
@@ -12,30 +12,25 @@ increase.forEach(function(hello){
         // } else{
         //     res1.style.color = "red"
         // }
-        result.innerHTML++;
-        if (result.innerHTML > 0){
-            result.style.color = "blue"
+       var ress = hello.parentElement.children[1];
+       ress.innerHTML++;
+        if (ress.innerHTML > 0){
+            ress.style.color = "blue"
         } else{
-            result.style.color = "red"
+            ress.style.color = "red"
         }
     })
 });
 
 decrease.forEach(function(hello2){
-    hello2.addEventListener('click', function(){
-    //    var res = e.target.parentElement.previousElementSibling;
-    //    console.log(res.innerHTML);
-    //    res--;
-    // //    if (res > 0){
-    // //        res.style.color = "blue"
-    // //    } else{
-    // //        res.style.color = "red"
-    // //    }
-    result.innerHTML = result.innerHTML - 1;
-    if (result.innerHTML > 0){
-        result.style.color = "blue"
+    hello2.addEventListener('click', function(e){
+       var res = e.target.parentElement.previousElementSibling;
+       res.innerHTML  = res.innerHTML - 1;
+       console.log(res);
+    if (res.innerHTML > 0){
+        res.style.color = "blue"
     } else{
-        result.style.color = "red"
+        res.style.color = "red"
     }
   })
 })
@@ -43,63 +38,80 @@ decrease.forEach(function(hello2){
 
 
 
-var reply = document.querySelector('.btn');
+var reply = document.querySelectorAll('.btn');
 var form = document.querySelector('.quote');
 
-reply.addEventListener('click', function(){
+reply.forEach(function(replybtn){
+    replybtn.addEventListener('click', function(e){
+    console.log(e.target.parentElement.parentElement.nextElementSibling.children[1]);
+        var longie = e.target.parentElement.parentElement.nextElementSibling.children[1];
+        if (longie.style.display == "none"){
+            longie.style.display = "block";
+            longie.classList.add('work');
+            } else {
+                longie.style.display = "none";
+        
+                longie.classList.add('work');
+            }
+
+
     // form.style.display = 'block';
     // form.classList.add('work');
     // console.log('hello')
-    if (form.style.display == "none"){
-        form.style.display = "block";
-        form.classList.add('work');
-    } else {
-        form.style.display = "none";
+
+    // if (form.style.display == "none"){
+    //     form.style.display = "block";
+    //     form.classList.add('work');
+    // } else {
+    //     form.style.display = "none";
+
         // form.classList.add('work');
-    }
-})
-
-
-var image1 = document.querySelector('.image');
-var hello = document.querySelector('.hello');
-var form1 = document.forms[0];
-form1.addEventListener('submit', function(e){
-    e.preventDefault();
-    var newText = form1.querySelector('input[type="text"]').value;
-    var p = document.createElement('p');
-    var div = document.createElement('div');
-    var span = document.createElement('span')
-    var img = document.createElement('img');
-
-    div.appendChild(img);
-    div.appendChild(p);
-    div.appendChild(span)
-    hello.appendChild(div);
-
-    p.textContent = newText;
-    img.src = "images/avatars/image-juliusomo.png"
-    span.textContent = 'Delete'
-
-    div.classList.add("newDiv");
-    span.classList.add('deleteSpan')
-
-
-
-    form1.reset();
-})
-
-
-var spann = document.querySelectorAll('.deleteSpan');
-spann.forEach(function(all){
-    all.addEventListener('click', function(e){
-        // all.style.backgroundColor = 'blue';
-        // console.log(all.innerHTML);
-    if (e.target.classList == 'deleteSpan'){
-        // const par = e.target.parentElement;
-        // hello.removeChild(par);
-        // console.log('working');
-        e.target.style.backgroundColor = 'blue';
-        console.log(e.target.innerHTML);
-    }
+    // }
 })
 })
+
+
+
+// var image1 = document.querySelector('.image');
+// var hello = document.querySelector('.hello');
+// var form1 = document.forms[0];
+// form1.addEventListener('submit', function(e){
+//     e.preventDefault();
+//     var newText = form1.querySelector('input[type="text"]').value;
+//     var p = document.createElement('p');
+//     var div = document.createElement('div');
+//     var span = document.createElement('span')
+//     var img = document.createElement('img');
+
+//     div.appendChild(img);
+//     div.appendChild(p);
+//     div.appendChild(span)
+//     hello.appendChild(div);
+
+//     p.textContent = newText;
+//     img.src = "images/avatars/image-juliusomo.png"
+//     span.textContent = 'Delete'
+
+//     div.classList.add("newDiv");
+//     span.classList.add('deleteSpan')
+
+
+
+//     form1.reset();
+// })
+
+
+// var spann = document.querySelectorAll('.deleteSpan');
+// spann.forEach(function(all){
+//     all.addEventListener('click', function(e){
+//         // all.style.backgroundColor = 'blue';
+//         // console.log(all.innerHTML);
+//     if (e.target.classList == 'deleteSpan'){
+//         // const par = e.target.parentElement;
+//         // hello.removeChild(par);
+//         // console.log('working');
+//         e.target.style.backgroundColor = 'blue';
+//         console.log(e.target.innerHTML);
+//     }
+// })
+// })
--- test.js
+++ test.js
@@ -1,14 +1,20 @@
-const btn = document.getElementById('btn');
+const btn = document.querySelectorAll('button');
+console.log(btn)
+// for (i = 0; i < btn.length; i++){
+//   btn[i].style.backgroundColor = 'red'
+// }
+btn.forEach(function(e){
+  e.style.backgroundColor = 'red';
+})
+// btn.addEventListener('click', function handleClick(event) {
+//   // 👇️ if you are submitting a form (prevents page reload)
+//   event.preventDefault();
 
-btn.addEventListener('click', function handleClick(event) {
-  // 👇️ if you are submitting a form (prevents page reload)
-  event.preventDefault();
+//   const firstNameInput = document.getElementById('first_name');
 
-  const firstNameInput = document.getElementById('first_name');
+//   // Send value to server
+//   console.log(firstNameInput.value);
 
-  // Send value to server
-  console.log(firstNameInput.value);
-
-  // 👇️ clear input field
-  firstNameInput.value = '';
-});
+//   // 👇️ clear input field
+//   firstNameInput.value = '';
+// });
