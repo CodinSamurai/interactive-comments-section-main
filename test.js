ba9dd8e89ffed2af5c5fbfa516afa4bce2e74cbe
TonyBod <bodudeanthony@gmail.com>
2022-05-02 15:59:45 -0700

    outline

--- test.js
+++ test.js
@@ -18,3 +18,42 @@ btn.forEach(function(e){
 //   // 👇️ clear input field
 //   firstNameInput.value = '';
 // });
+$('h1').css('color', 'red');
+$('h2').addClass('jqueryy');
+console.log($('h2'))
+
+// $(document).keypress(function(e){
+//   console.log(e.target);
+//   $('h1').text(e.key);
+// });
+
+$('h1').on('mouseenter',function(){
+  $('h2').css('color', 'yellow')
+})
+
+$('h2').click(function(){
+$('h2').css('color', 'red')
+$('h2').slideUp().slideDown();
+})
+
+// $('button').click(function(){
+//   $(this).text('wait please');
+//   $(this).css('backgroundColor', 'green');
+//   $(this).attr('disabled', true);
+//   setTimeout(() => {
+//     $(this).attr('disabled', false);
+//     $(this).text('submit');
+//     $(this).css('backgroundColor', 'red');
+//   }, 1000);
+// })
+
+$('button').click(function(e){
+  $(e.target).text('wait please');
+  $(e.target).css('backgroundColor', 'green');
+  $(e.target).attr('disabled', true);
+  setTimeout(() => {
+    $(e.target).attr('disabled', false);
+    $(e.target).text('submit');
+    $(e.target).css('backgroundColor', 'red');
+  }, 1000);
+})
